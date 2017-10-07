61fec3636a63e99355a2e9c8adec018e12919000
Olena Poliakova <olena_poliakova@Olenas-MacBook-Air.local>
2017-10-07 17:42:17 +0300

    Add API for creating, deleting and updating database

--- src/main/java/database/Schema.java
+++ src/main/java/database/Schema.java
@@ -2,8 +2,61 @@ package database;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Schema {
     private String name;
-    private Map<String, Table> tables = new HashMap<>();
+    private Map<String, Table> tables;
+
+    public Schema(String name) {
+        this.name = name;
+        tables = new HashMap<>();
+        //TODO: write into database
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Map<String, Table> getTables() {
+        return tables;
+    }
+
+    public void addTable(String name, Table table){
+        if (tables.get(name) != null){
+            throw new IllegalArgumentException("table with name " + name + " already exists");
+        }
+        tables.put(name, table);
+        //TODO: write into database
+    }
+
+    public void addTable(String name, Set<Attribute> attributes){
+        if (tables.get(name) != null){
+            throw new IllegalArgumentException("table with name " + name + " already exists");
+        }
+        tables.put(name, new Table(name, attributes));
+        //TODO: write into database
+    }
+
+    public void deleteTable(String name){
+        if (tables.get(name) == null){
+            throw new IllegalArgumentException("table with name " + name + "does not exists");
+        }
+        //TODO: delete from database
+    }
+
+    public Table readTableFromDatabase(String name){
+        if (tables.get(name) == null){
+            throw new IllegalArgumentException("table with name " + name + "does not exists");
+        }
+        //TODO: get from database
+        return null;
+    }
+
+    public void writeTableToDatabase(String name){
+        if (tables.get(name) == null){
+            throw new IllegalArgumentException("table with name " + name + "does not exists");
+        }
+        //TODO: put into database
+    }
 }
