2eee514d763689343f1f7200e2876a3366f72d3f
Olena Poliakova <olena_poliakova@Olenas-MacBook-Air.local>
2017-10-07 09:03:41 +0300

    Fix Table

--- src/main/java/database/Table.java
+++ src/main/java/database/Table.java
@@ -2,35 +2,39 @@ package database;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class Table {
     private final String name;
-    private final List<DatabaseTypes> types;
+    private final Set<Attribute> attributes;
     private List<Entry> entries;
 
-    public Table(String name, List<DatabaseTypes> types) {
+    public Table(String name, Set<Attribute> attributes) {
         this.name = name;
-        this.types = types;
-        this.entries = new ArrayList<>(E);
+        this.attributes = attributes;
+        this.entries = new ArrayList<>();
     }
 
     public String getName() {
         return name;
     }
 
-    public List<DatabaseTypes> getTypes() {
-        return types;
+    public Set<Attribute> getAttributes() {
+        return attributes;
     }
 
     public List<Entry> getEntries() {
         return entries;
     }
 
-    public void addEntry(Entry entry) {
-
+    public void addEntry(Entry newEntry) {
+        if (checkAttributes(newEntry.getAttributes())) {
+            throw new IllegalArgumentException("new entry and table have incompatible types");
+        }
+        entries.add(newEntry);
     }
 
-    private boolean checkAttributes(List<DatabaseTypes> entryTypes){
-        return types.containsAll(entryTypes) && entryTypes.containsAll(types);
+    private boolean checkAttributes(Set<Attribute> entryAttributes){
+        return attributes.containsAll(entryAttributes) && entryAttributes.containsAll(attributes);
     }
 }
