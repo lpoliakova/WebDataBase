218a61eb4e74dcb5f557603d3b902a066472a34e
Olena Radchykova <oradchykova@Oleksandrs-iMac.local>
2017-10-25 21:13:03 +0300

    Add test utils. Test refactoring

--- src/test/java/ioTests/XmlTest.java
+++ src/test/java/ioTests/XmlTest.java
@@ -3,35 +3,18 @@ package ioTests;
 import database.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import utils.TableTestExamples;
 
 import java.util.*;
 
 public class XmlTest {
     @Test
     void writeReadTableTest() {
-        Schema company = new Schema("Company");
-        Table people = createTestTable();
+        Schema company = Schema.createSchema("Company");
+        Table people = TableTestExamples.createMikeJimTable();
         company.addTable(people.getName(), people);
 
         Table peopleFromDb = company.readTableFromDatabase(people.getName());
         Assertions.assertEquals(people, peopleFromDb);
-    }
-
-    private Table createTestTable() {
-        Set<Attribute> attributes = new HashSet<>();
-        Attribute name = new Attribute("name", DatabaseTypes.CHAR);
-        Attribute age = new Attribute("age", DatabaseTypes.INT);
-        attributes.add(name);
-        attributes.add(age);
-        Table people = new Table("People", attributes);
-        Map<Attribute, String> mikeAttributes = new HashMap<>();
-        mikeAttributes.put(name, "Mike");
-        mikeAttributes.put(age, "21");
-        people.addEntry(new Entry(mikeAttributes));
-        Map<Attribute, String> jimAttributes = new HashMap<>();
-        jimAttributes.put(name, "Jim");
-        jimAttributes.put(age, "35");
-        people.addEntry(new Entry(jimAttributes));
-        return people;
     }
 }
---
+++ src/test/java/utils/TableTestExamples.java
@@ -0,0 +1,61 @@
+package utils;
+
+import database.Attribute;
+import database.DatabaseTypes;
+import database.Entry;
+import database.Table;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by oradchykova on 10/25/17.
+ */
+public class TableTestExamples {
+    public static Table createJimTable() {
+        Table people = new Table("Jim", createPeopleAttributes());
+        addPerson(people, "Jim", 35);
+        return people;
+    }
+
+    public static Table createMikeTable() {
+        Table people = new Table("Mike", createPeopleAttributes());
+        addPerson(people, "Mike", 21);
+        return people;
+    }
+
+    public static Table createMikeJimTable() {
+        Table people = new Table("MikeJim", createPeopleAttributes());
+        addPerson(people, "Mike", 21);
+        addPerson(people, "Jim", 35);
+        return people;
+    }
+
+    public static Table createArturMikeTable() {
+        Table people = new Table("ArturMike", createPeopleAttributes());
+        addPerson(people, "Artur", 15);
+        addPerson(people, "Mike", 21);
+        return people;
+    }
+
+    private static void addPerson(Table table, String name, Integer age) {
+        Map<Attribute, String> personAttributes = new HashMap<>();
+        personAttributes.put(createAttribute("name", DatabaseTypes.CHAR), name);
+        personAttributes.put(createAttribute("age", DatabaseTypes.INT), age.toString());
+        table.addEntry(Entry.create(personAttributes));
+    }
+
+    private static Set<Attribute> createPeopleAttributes() {
+        Set<Attribute> attributes = new HashSet<>();
+        attributes.add(createAttribute("name", DatabaseTypes.CHAR));
+        attributes.add(createAttribute("age", DatabaseTypes.INT));
+        return attributes;
+    }
+
+    private static Attribute createAttribute(String name, DatabaseTypes type) {
+        return new Attribute(name, type);
+    }
+
+}
