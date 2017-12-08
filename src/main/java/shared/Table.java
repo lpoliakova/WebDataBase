17b23e95981e2d7e32dad26c835c873e2ecb0889
lpoliakova <olena.poliakova@gmail.com>
2017-12-08 14:08:15 +0200

    Update jaxb annotations

--- src/main/java/shared/Table.java
+++ src/main/java/shared/Table.java
@@ -8,6 +8,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+@XmlRootElement
 public class Table implements Serializable{
     @XmlElement
     private final String name;
